3cf896797205988000950106924594ee40455721
AdamIzdebski <adam.izdebski1@gmail.com>
2025-12-10 14:33:51 +0100

    Add docking utilities and integrate RDKit and OpenBabel for molecular docking

--- src/joint_improvement/utils/chemistry/__init__.py
+++ src/joint_improvement/utils/chemistry/__init__.py
@@ -1,9 +1,12 @@
 """Chemistry utilities for molecular property calculations."""
 
+from .docking import calculate_docking, calculate_docking_batch
 from .logp import calculate_logp
 from .validity import calculate_validity
 
 __all__ = [
+    "calculate_docking",
+    "calculate_docking_batch",
     "calculate_logp",
     "calculate_validity",
 ]
