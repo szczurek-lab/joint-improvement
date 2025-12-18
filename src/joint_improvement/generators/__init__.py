dc255de95c6d60e4d015d82d5a85ec142c6bd027
AdamIzdebski <adam.izdebski1@gmail.com>
2025-12-18 09:55:18 +0100

    Enhance SequenceDataset's append method to validate multi-target sequences and ensure type consistency

--- src/joint_improvement/generators/__init__.py
+++ src/joint_improvement/generators/__init__.py
@@ -1,10 +1,11 @@
-from .gumbeldore import GumbeldoreMixinV1, GumbeldoreMixinV2
+from .gumbeldore import GumbeldoreMixin, GumbeldoreMixinV1, GumbeldoreMixinV2
 from .tasar import TasarMixin, TasarMixinLegacy
 from .unconditional import UnconditionalGeneratorMixin
 
 __all__ = [
     "TasarMixin",
     "TasarMixinLegacy",
+    "GumbeldoreMixin",
     "GumbeldoreMixinV1",
     "GumbeldoreMixinV2",
     "UnconditionalGeneratorMixin",
